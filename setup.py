1dd6f84282241af450e2ebd449b62af417937630
David <pydavid@users.noreply.github.com>
2017-07-24 09:51:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ from setuptools import find_packages, setup
 setup(
     name='baguette-client',
     version='0.1',
-    url='baguette.io',
-    download_url='baguette.io/get',
-    author_email='dev@baguette.io',
+    description='Baguette Command Line Interface',
+    long_description=open('README.rst').read(),
+    keywords=['cli', 'baguette'],
+    url='https://github.com/baguette-io/baguette-client/',
+    author_email='pydavid@baguette.io',
+    classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: OS Independent',
+      ],
     packages=find_packages(),
-    platforms=[
-        'Linux/UNIX',
-        'MacOS',
-        'Windows'
-    ],
     install_requires=[
         'requests==2.10.0',
         'click==6.6',
