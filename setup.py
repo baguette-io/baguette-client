e4e01fc5afa13037224ef75367cdc9fea157d006
David Macro <david.macro7@gmail.com>
2018-01-21 11:45:56 +0100

    cosmetics

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
           'License :: OSI Approved',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Operating System :: OS Independent',
